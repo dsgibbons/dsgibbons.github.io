6831fe08220ab49c189a15d9d30ce60ee2da4cef
dsgibbons <git@gibbons.foo>
2025-03-10 22:40:02 +1030

    chore: update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "dsgibbons",
+	url: "https://blog.gibbons.foo",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Blog.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Daniel Gibbons",
+		email: "daniel@gibbons.foo",
+		url: "https://blog.gibbons.foo"
 	}
 }
